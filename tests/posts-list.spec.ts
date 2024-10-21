8b01747ad64ac4a82c15bc54533795dd37b2dc70
dd <dd5892631@gmail.com>
2024-10-21 20:30:53 +0300

    add tests

---
+++ tests/posts-list.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("create delete post list", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByPlaceholder("название").click();
+  await page.getByPlaceholder("название").fill("Test title");
+  await page.getByPlaceholder("описание").click();
+  await page.getByPlaceholder("описание").fill("Test content");
+  await page.getByRole("button", { name: "Добавить" }).click();
+  await expect(page.getByText("Test titleTest contentУдалить")).toBeVisible();
+
+  await page.getByRole("button", { name: "Удалить" }).click();
+
+  await expect(
+    page.getByText("Test titleTest contentУдалить"),
+  ).not.toBeVisible();
+});
