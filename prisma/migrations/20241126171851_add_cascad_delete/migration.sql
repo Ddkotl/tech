0f7777690c4db799e0d8a8b3baf7de1946b816b5
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2024-11-26 17:22:41 +0000

    fix

---
+++ prisma/migrations/20241126171851_add_cascad_delete/migration.sql
@@ -0,0 +1,32 @@
+-- DropForeignKey
+ALTER TABLE "bookmarks" DROP CONSTRAINT "bookmarks_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "bookmarks" DROP CONSTRAINT "bookmarks_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_category_id_fkey";
+
+-- AlterTable
+ALTER TABLE "posts" ALTER COLUMN "category_id" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
