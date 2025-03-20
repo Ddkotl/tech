3a58e62680f5ea4f02a0f8ff0f192a55f7fea318
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-03-20 18:00:30 +0000

    add tags list

--- src/entities/tag/_ui/tags_list.tsx
+++ src/entities/tag/_ui/tags_list.tsx
@@ -1,16 +1,46 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getAllTags } from "../_actions/get_all_tags";
-import { TagsWithCounts } from "../_domain/types";
 import { TagCard } from "./tag_card";
+import { Card, CardContent, CardHeader, Skeleton } from "@/shared/components";
 
 export function TagsList() {
-  const [tags, setTags] = useState<TagsWithCounts[] | undefined>();
+  const {
+    data: tags,
+    error,
+    isError,
+    isLoading,
+    isFetching,
+  } = useQuery({
+    queryKey: ["tags"],
+    queryFn: () => getAllTags(),
+  });
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (isLoading || isFetching) {
+    // Отображаем скелетоны
+    return (
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 lg:gap-4 auto-rows-fr">
+        {Array.from({ length: 20 }).map((_, i) => (
+          <Card
+            key={i} // Добавляем уникальный ключ для каждого элемента
+            className="p-0 h-24 flex flex-col justify-evenly shadow-md transition-all duration-300 hover:scale-95 hover:shadow-lg hover:bg-foreground/10 items-center"
+          >
+            <CardHeader className="p-1 flex items-center justify-center text-center">
+              <Skeleton className="w-24 h-4 rounded-md" />
+            </CardHeader>
+            <CardContent className="flex flex-col p-1 items-center justify-center gap-1">
+              <Skeleton className="w-24 h-3 rounded-md" />
+              <Skeleton className="w-20 h-3 rounded-md" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    );
+  }
 
-  useEffect(() => {
-    getAllTags().then((tagsList) => setTags(tagsList));
-  }, []);
   return (
     <div className=" grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 lg:gap-4 auto-rows-fr">
       {tags?.map((tag) => (
