1acff9bd217e800d39608cda377ce8a1c0a6a1a3
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-05-11 15:27:40 +0000

    fix

--- src/widgets/app-header/app-header.tsx
+++ src/widgets/app-header/app-header.tsx
@@ -3,13 +3,12 @@ import { Logo } from "../../shared/components/custom/logo";
 import { MainNav } from "./_ui/main-nav";
 import { mainNavItems } from "./_settings/main-nav-items";
 import { MobileMenu } from "./_ui/mobile-menu";
-import { Profile } from "../../shared/components/custom/profile";
 import { ToggleTheme } from "@/features/theme/toggle-theme";
 import { Actions } from "./_ui/actions";
 import { Container } from "@/shared/components";
 import { BookmarksIcon } from "@/features/bookmarks/ui/bookmarks_icon";
 
-export function AppHeader({ variant }: { variant: "auth" | "private" | "public" | "admin" }) {
+export function AppHeader() {
   // const isProfile = variant !== "auth";
 
   return (
