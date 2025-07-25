e0b73a3ccd53a03ea9b325ef17de19793bf7871a
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-07-25 05:38:19 +0000

    add parse timeout

--- src/shared/lib/parsing/index.ts
+++ src/shared/lib/parsing/index.ts
@@ -5,7 +5,21 @@ import { parseReviewsFromManyPages } from "./modules/get_reviews_with_tag_from_m
 import { getAllBrandsAndModels } from "./modules/get_all_brands_and_models";
 import { addHTTPheaders } from "./functions/addHTTPheaders";
 
-export async function StartParse() {
+export async function StartParse(){
+  const timeoutPromise= new Promise((_,rej)=>{
+    setTimeout(()=>rej(new Error("Tech parse time out after 5 hours")),5*60*60*1000)
+  })
+  try {
+    Promise.race([
+      ExeParse(),
+      timeoutPromise
+    ])
+  } catch (error) {
+    console.error("Error in start parse",error)
+  }
+}
+
+async function ExeParse() {
   let browser: Browser | undefined;
   try {
     browser = await chromium.launch({ headless: true });
