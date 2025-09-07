bcd839c81f1237b5c550fc44dc6aa5b21ae88961
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-09-07 21:22:35 +0300

    Update ai_client.ts

--- src/shared/lib/parsing/openai/ai_client.ts
+++ src/shared/lib/parsing/openai/ai_client.ts
@@ -5,5 +5,5 @@ export const client = new OpenAI({
   baseURL: "http://localhost:1337/v1",
 });
 
-export const TEXT_AI_MODEL = "gpt-4o-mini";
+export const TEXT_AI_MODEL = "deepseek-r1";
 export const IMAGE_AI_MODEL = "flux";
